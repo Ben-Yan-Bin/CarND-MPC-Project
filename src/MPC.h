870c31fb3a50285351d6361bdeaff281e292d908
Ben-Yan-Bin <33827934+Ben-Yan-Bin@users.noreply.github.com>
2018-11-24 21:43:12 +0800

    Update MPC.h

--- src/MPC.h
+++ src/MPC.h
@@ -4,6 +4,24 @@
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 
+using namespace std;
+
+const size_t N = 10;
+const double dt = 0.1;
+const int latency_ind = 1; //latency  in units of dt (100ms)
+
+struct Solution {
+
+		vector<double> X;
+		vector<double> Y;
+		vector<double> Psi;
+		vector<double> V;
+		vector<double> Cte;
+		vector<double> EPsi;
+		vector<double> Delta;
+		vector<double> A;
+};
+
 class MPC {
  public:
   MPC();
@@ -11,8 +29,14 @@ class MPC {
   virtual ~MPC();
 
   // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuations.
-  std::vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  // Return the first actuatotions.
+  //vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  Solution Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+
+  double delta_prev {0};
+  double a_prev {0.1};
+
 };
+
 
 #endif /* MPC_H */
